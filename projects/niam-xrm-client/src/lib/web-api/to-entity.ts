a7a5459da0b442eecea248e3b7e5db149889101e
Khairuddin Ni'am <niam.nfinity@gmail.com>
2020-06-07 05:56:54 +0700

    Fix to-entity

--- projects/niam-xrm-client/src/lib/web-api/to-entity.ts
+++ projects/niam-xrm-client/src/lib/web-api/to-entity.ts
@@ -1,42 +1,45 @@
 import { EntityMetadata } from '../../main';
 import { Entity, EntityAttributeValue } from '../definitions';
 
+interface Attribute {
+  name: string;
+  value: EntityAttributeValue
+}
+
 export function toEntity<TEntity extends Entity>(entityMetadata: EntityMetadata, webApiRetrieveResponse: { [k: string]: any }): TEntity {
   const keys = Object.keys(webApiRetrieveResponse);
   const names = keys.filter(k => k.indexOf('@') === -1); // Ignore metadata properties
-  const result: Entity = {};
-
-  for (const name of names) {
-    const attributeName = getAttributeName(name);
-    result[attributeName] = getAttributeValue(entityMetadata, webApiRetrieveResponse, attributeName);
-  }
-
+  const result = names
+    .map(name => getAttribute(entityMetadata, webApiRetrieveResponse, name))
+    .reduce((e, attribute) => {
+      e[attribute.name] = attribute.value;
+      return e;
+    }, {} as Entity);
   return result as TEntity;
 }
 
-function getAttributeName(name: string): string {
-  const isLookup = name.startsWith('_') && name.endsWith('_value');
-  return isLookup ? name.substr(1, name.length - 7) : name;
-}
-
-function getAttributeValue(entityMetadata: EntityMetadata, webApiRetrieveResponse: { [k: string]: any }, attributeName: string): EntityAttributeValue {
+function getAttribute(entityMetadata: EntityMetadata, webApiRetrieveResponse: { [k: string]: any }, name: string): Attribute {
+  const attributeName = getAttributeName(name);
   const attributeMetadata = entityMetadata.attributes.find(md => md.logicalName === attributeName);
-
   switch (attributeMetadata.attributeType) {
     case 'lookup':
     case 'owner':
       const lookupValue: Xrm.LookupValue = {
         id: webApiRetrieveResponse[name],
         name: webApiRetrieveResponse[`${name}@OData.Community.Display.V1.FormattedValue`],
-        // TODO: is entityType objectytpecode or logicalname ?
         entityType: webApiRetrieveResponse[`${name}@Microsoft.Dynamics.CRM.lookuplogicalname`],
       };
-      return [lookupValue];
+      return { name: attributeName, value: [lookupValue] };
     
     case 'datetime':
-      return new Date(webApiRetrieveResponse[name]);
+      return { name: attributeName, value: new Date(webApiRetrieveResponse[name]) };
 
     default:
-      return webApiRetrieveResponse[name];
+      return { name: attributeName, value: webApiRetrieveResponse[name] };
   }
+}
+
+function getAttributeName(name: string): string {
+  const isLookup = name.startsWith('_') && name.endsWith('_value');
+  return isLookup ? name.substr(1, name.length - 7) : name;
 }
